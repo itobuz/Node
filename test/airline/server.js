28ec2f360432c3d4d054d6839fd96582204a9b4b
sagar <sagarsneh@gmail.com>
2013-12-02 02:58:58 +0530

    optimist

--- test/airline/server.js
+++ test/airline/server.js
@@ -2,12 +2,16 @@ var http = require('http'),
 	flights = require("./data"),
 	db = require('./db'),
 	repl = require('repl'),
+	argv = require('optimist').argv,
 	app = require('./app')(flights, db);
 	
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-var prompt = repl.start({prompt: 'flights> '});
+//var prompt = repl.start({prompt: 'flights> '});
 
-prompt.context.data = flights;
+//prompt.context.data = flights;
+
+console.log(argv);
+console.log(process.argv);
