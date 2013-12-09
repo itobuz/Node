67612bb2728f245756e4514cd35a4229cb3f6629
sagar <sagarsneh@gmail.com>
2013-12-10 03:55:06 +0530

    stream.js added with basic stream functionality

---
+++ test/airline/stream.js
@@ -0,0 +1,17 @@
+var fs = require('fs');
+
+var stream = fs.createReadStream('package.json');
+
+stream.on('data', function (chunk) {
+	console.log('----------------------begin chunk --------------');
+	console.log(chunk.toString());
+	console.log('-----------------------end chunk ---------------');
+});
+
+stream.on('data', function (chunk) {
+	console.log('CHUNK LENGTH WAS: ' + chunk.length);
+});
+
+stream.on('end', function () {
+	console.log('----------------------reached file end-------------');
+})
